0dd4977f6f19f2c51e4e0d638e2920cbc6f5de41
Ibnu Said M <83802831+ibnusm@users.noreply.github.com>
2022-07-12 15:12:42 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,20 @@
+#ChinuLz Ganteng
+#ChinuLz Ganteng
+#ChinuLz Ganteng
+#ChinuLz Ganteng
+#ChinuLz Ganteng
+
+echo ""
+echo -e "\e[32minstalling requirements"
+echo -e "\e[37m"
+apt-get install -y python
+pip install requests
+pip install datetime
+pip install printtools
+echo ""
+echo -e "\e[32m[*] Done! Run This Code => python graber.py"
+echo ""
+echo "wait 5s..."
+sleep 5
+clear
+python graber.pyc
